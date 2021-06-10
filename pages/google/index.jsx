086f3e0ce8237d08852411954816d6668c94d032
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-06-10 15:40:12 +0530

    :coffin: removed: Useless code

--- pages/google/index.jsx
+++
@@ -1,5 +0,0 @@
-import DriveLinkGenerator from "./Drive/linkGenerator";
-import YoutubeDownloader from "./YouTube/downloader";
-// import YoutubeVideoDownloader from "./YouTube/videoDownloader";
-
-export { DriveLinkGenerator, YoutubeDownloader };
